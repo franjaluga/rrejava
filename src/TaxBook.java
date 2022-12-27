5de9cc651b9333e51d33abcdd30e186696dd568f
franjaluga <playaimpuestos@gmail.com>
2022-12-27 17:06:39 -0300

    v.19(beauty, unestable (final part)

--- src/TaxBook.java
+++ src/TaxBook.java
@@ -32,6 +32,32 @@ public class TaxBook{
 
     }
 
+    public void updateAndPrintBalance(){
+        updateBalance();
+        printBalanceUpdated();
+    }
+
+    public void updateBalance(){
+        rai.setReversoSaldo(rai.getSaldoReajustado());
+        rai.setSaldoAntesDeDistribuciones(rai.getAumentosDelEjercicio());
+        sac.setSaldoAntesDeDistribuciones(sac.getSaldoReajustado() + sac.getAumentosDelEjercicio());
+    }
+
+    public void printBalanceUpdated(){
+        System.out.println("|===================|=============|==============|");
+        System.out.println("| Operaciones       |    RAI      |    SAC       |");
+        System.out.println("|===================|=============|==============|");
+        System.out.println("| RAI(01.01.2022)   |"+Funciones.rellenar(rai.getSaldoInicial()) +" | "+Funciones.rellenar(sac.getSaldoInicial())+" |");
+        System.out.println("| RAI(Reajuste)     |"+Funciones.rellenar(rai.getReajuste())+" | "+Funciones.rellenar(sac.getReajuste())+" |");
+        System.out.println("| RAI Reajustado    |"+Funciones.rellenar(rai.getSaldoReajustado())+" | "+Funciones.rellenar(sac.getSaldoReajustado())+" |");
+        System.out.println("| Reverso RAI       |"+Funciones.rellenar(-rai.getReversoSaldo())+" | "+Funciones.rellenar(0)+" |");
+        System.out.println("| SAC del Año       |"+Funciones.rellenar(0)+" | "+Funciones.rellenar(sac.getAumentosDelEjercicio())+" |");
+        System.out.println("| RAI del Año       |"+Funciones.rellenar(rai.getAumentosDelEjercicio())+" | "+Funciones.rellenar(0)+" |");
+        System.out.println("|===================|=============|==============|");
+        System.out.println("| RAI ant. dist.    |"+Funciones.rellenar(rai.getSaldoAntesDeDistribuciones())+" | "+Funciones.rellenar(sac.getSaldoAntesDeDistribuciones())+" |");
+        System.out.println("|===================|=============|==============|");
+    }
+
     public void case2(){
         System.out.println("Seleccionó ingresar RAI y SAC al 31.12.2022");
         System.out.println("Ingrese RAI final");
@@ -44,41 +70,23 @@ public class TaxBook{
     }
 
 
-    public void updateBalance(){
-        rai.setReversoSaldo(rai.getSaldoReajustado());
-        rai.setSaldoAntesDeDistribuciones(rai.getAumentosDelEjercicio());
-        sac.setSaldoAntesDeDistribuciones(sac.getSaldoReajustado() + sac.getAumentosDelEjercicio());
-    }
-
-    public void printBalanceUpdated(){
-        System.out.println("| RAI(01.01.2022): "+rai.getSaldoInicial()+" | SAC(01.01.2022): "+sac.getSaldoInicial()+" |");
-        System.out.println("| RAI(Reajuste): "+rai.getReajuste()+" | SAC(Reajuste): "+sac.getReajuste()+" |");
-        System.out.println("| RAI REVERSO: "+-rai.getReversoSaldo());
-        System.out.println("| SAC del Año: "+"----------------"+sac.getAumentosDelEjercicio());
-        System.out.println("| RAI(ant. de dist.: "+rai.getSaldoAntesDeDistribuciones()+" | SAC(ant. de dist.: "+sac.getSaldoAntesDeDistribuciones()+" |");
-    }
-
-    public void updateAndPrintBalance(){
-        updateBalance();
-        printBalanceUpdated();
-    }
-
     public void case3(){
         System.out.println("Seleccionó ingresar distribuciones");
         dist.setDistribucionesMensuales();
-        System.out.println("Se ingresaron: "+ dist.getDistribucionesTotales());
-
+        System.out.println("|================================================|");
+        System.out.println("|  Se ingresaron distribuciones por : "+ dist.getDistribucionesTotales());
+        System.out.println("|================================================|");
     }
 
     public void case5(){
-        // LOGICA DE PROCESAR LOS RETIROS
+
         System.out.println("Seleccionó generar RRE");
 
         dist.procesarDistribuciones();
 
-        System.out.println("DIST. REAJ: "+dist.sumaDistribucionesReajustadas());
         imputacionesAlSac();
         imputacionesAlRai();
+        printReporte();
     }
 
     public void imputacionesAlSac(){
@@ -90,30 +98,49 @@ public class TaxBook{
         }
 
         sac.setSaldoFinal(sac.getSaldoAntesDeDistribuciones() - sac.getImputacionesDelEjercicio());
-
-        System.out.println("SAC->IMPUTACIONES DEL EJ. : "+sac.getImputacionesDelEjercicio());
-        System.out.println("SAC->SALDO FINAL DEL EJ.  : "+sac.getSaldoFinal());
     }
 
     public void imputacionesAlRai() {
 
         int saldoRaiPreCierre = rai.getSaldoAntesDeDistribuciones() - dist.getDistribucionesTotalesReajustadas();
         rai.setDistribuciones_no_imputadas(Math.abs(saldoRaiPreCierre));
-        System.out.println("RAI->PRE-CIERRE: "+saldoRaiPreCierre);
 
         if(saldoRaiPreCierre < 0 ){
             rai.saldoFinal = 0;
         }
 
-        System.out.println("RAI->RECALCULAR-CIERRE: "+ rai.saldoFinal);
-        System.out.println("DISTR. NO REAJ.: "+dist.getDistribucionesTotales());
-        System.out.println("DISTR. REAJ->  : "+dist.getDistribucionesTotalesReajustadas() );
-
-        System.out.println(("=================="));
-
-        System.out.println("RAI IMPUTADO   :"+rai.getImputacionesDelEjercicio());
-        System.out.println("RAI SALDO FINAL: "+rai.getSaldoFinal());
-        System.out.println("RAI NO IMPUT   :"+rai.getDistribuciones_no_imputadas());
 
     }
+
+    public void printReporte(){
+        updateAndPrintBalance();
+        printImputaciones();
+    }
+
+    public void printImputaciones(){
+
+        if(DistributionIsBiggerThanBalance()){
+            System.out.println(DistributionIsBiggerThanBalance());
+            System.out.println("| RAI IMPUTADO      |"+Funciones.rellenar(rai.getSaldoAntesDeDistribuciones())+" | "+Funciones.rellenar(sac.getImputacionesDelEjercicio())+" |");
+        }else{
+            System.out.println(DistributionIsBiggerThanBalance());
+            System.out.println("| RAI IMPUTADO      |"+Funciones.rellenar(dist.getDistribucionesTotalesReajustadas())+" | "+Funciones.rellenar(sac.getImputacionesDelEjercicio())+" |");
+        }
+
+        System.out.println("|===================|=============|==============|");
+        System.out.println("| SALDO             |"+Funciones.rellenar(rai.getSaldoFinal())+" | "+Funciones.rellenar(sac.getSaldoFinal())+" |");
+        System.out.println("|===================|=============|==============|");
+        //System.out.println("|................................................|");
+        //System.out.println("| RAI NO IMPUT      |"+Funciones.rellenar(rai.getDistribuciones_no_imputadas())+" | ");
+        //System.out.println("|................................................|");
+    }
+
+    public boolean DistributionIsBiggerThanBalance(){
+        if(dist.getDistribucionesTotalesReajustadas() > rai.getSaldoAntesDeDistribuciones()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
